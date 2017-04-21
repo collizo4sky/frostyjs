faf6f7ef0645ce61839e0b301e3f5c8c4b8d4e59
Kai Falkowski <kai.falkowski@gmail.com>
2017-04-21 15:20:03 +0200

    work over smart-reposition feature: - consider both positions on an axis are leaving the viewport - adjust condition for top overflow test

--- src/js/frosty.js
+++ src/js/frosty.js
@@ -9,7 +9,7 @@
  *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
  *  and associated documentation files (the "Software"), to deal in the Software without restriction,
  *  including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *  and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+ *  and/or sell copies of the Software, and to permit persons to whom the Software is furni shed to do so, 
  *  subject to the following conditions:
  *  
  *  The above copyright notice and this permission notice shall be included in all copies or 
@@ -170,19 +170,36 @@
             return coords;
         },
 
+        _getNewCoords: function() {
+            var coords = this._getPosition();
+            this.$el.attr('class', this.options.classes);
+            this._addArrowClass();
+            return coords;
+        },
+
         _checkOverflow: function(coords) {
             if (!this.options.smartReposition) { return coords; }
             var originalPosition = this.options.position;
-            
-            if (coords.top < 0) { this.options.position = 'bottom'; }
-            if (coords.top + this.$el.height() > $(window).height()) { this.options.position = 'top'; }
-            if (coords.left < 0) { this.options.position = 'right'; }
-            if (coords.left + this.$el.width() > $(window).width()) { this.options.position = 'left'; }
+            var overflowSpotted = {};
 
-            if (this.options.position !== originalPosition) {
-                coords = this._getPosition();
-                this.$el.attr('class', this.options.classes);
-                this._addArrowClass();
+            // check overflow left & top
+            if (coords.top < 0) { this.options.position = 'bottom'; overflowSpotted.top = true; }
+            if (coords.left < 0) { this.options.position = 'right'; overflowSpotted.left = true; }
+            if (this.options.position !== originalPosition) { coords = this._getNewCoords(); }
+            
+            // check overflow right & bottom
+            if (coords.top + this.$el.height() > $(window).height() + $(document).scrollTop()) { this.options.position = 'top'; overflowSpotted.bottom = true; }
+            if (coords.left + this.$el.width() > $(window).width()) { this.options.position = 'left'; overflowSpotted.right = true; }
+            if (this.options.position !== originalPosition) { coords = this._getNewCoords(); }
+
+            // exit if overflow at all positions
+            if (Object.keys(overflowSpotted).length == 4) { console.error('frosty tootlip has no space to get opened!'); return coords; }
+
+            // check overflow on the same axis
+            if (Object.keys(overflowSpotted).length == 2) {
+                if (overflowSpotted.left && overflowSpotted.right) { this.options.position = 'top'; }
+                if (overflowSpotted.top && overflowSpotted.bottom) { this.options.position = 'right'; }
+                coords = this._getNewCoords();
             }
 
             return coords;
